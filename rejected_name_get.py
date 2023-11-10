8f04e866cd1f88c529ffddbea9f993cc280d2894
a-mcculley <150435974+a-mcculley@users.noreply.github.com>
2023-11-10 05:45:41 -0500

    Update rejected_name_get.py back to the currently used version

--- rejected_name_get.py
+++ rejected_name_get.py
@@ -1,18 +1,32 @@
 f = open("elfnames_full.txt", "r")
 g = open("elfnames.txt", "r")
-#also open any previous rejected lists
+h = open("elfnames_rejectedv1.txt", "r")
+i = open("elfnames_rejectedv2.txt", "r")
+j = open("elfnames_rejectedv3.txt", "r")
+k = open("elfnames_rejectedv4.txt", "r")
+l = open("elfnames_rejectedv5.txt", "r")
+m = open("elfnames_rejectedv6.txt", "r")
+n = open("elfnames_rejectedv7.txt", "r")
+o = open("elfnames_rejectedv8.txt", "r")
+p = open("elfnames_rejectedv9.txt", "r")
 
 list1 = f.readlines() 
 list2 = g.readlines()
-#also call readlines to create a list3 (or higher) based on previous rejected lists
+list3 = h.readlines()
+list4 = i.readlines()
+list5 = j.readlines()
+list6 = k.readlines()
+list7 = l.readlines()
+list8 = m.readlines()
+list9 = n.readlines()
+list10 = o.readlines()
+list11 = p.readlines()
 
 outlist = []
 for el in list1 :
-    #add "and el not in list3" for the previous rejected iterations (and increment beyond 3 as needed)
-    if el not in list2:
+    if el not in list2 and el not in list3 and el not in list4 and el not in list5 and el not in list6 and el not in list7 and el not in list8 and el not in list9 and el not in list10 and el not in list11:
         outlist.append(el)
 
-#update the rejected version number
-z = open("elfnames_rejectedv1.txt", "w")
+z = open("elfnames_rejectedv10.txt", "w")
 for el in outlist:
     z.write(el)
